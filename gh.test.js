a0b22d5e26b248593daafcf273272636f573f141
natakozlova <natabalabanova555@yandex.ru>
2023-12-11 23:25:36 +0300

    fixed

--- gh.test.js
+++ gh.test.js
@@ -1,53 +1,42 @@
 const puppeteer = require("puppeteer");
 
+beforeEach(async () => {
+  page = await browser.newPage();
+  await page.goto("https://github.com/team");
+});
+
+afterEach(() => {
+  page.close();
+});
+
 let page;
 describe("Github page tests", () => {
-
-  beforeEach(async () => {
-    page = await browser.newPage();
-    await page.goto("https://github.com/team");
-  });
-  
-  afterEach(() => {
-    page.close();
-  });
-  
-
   test("The h1 header content'", async () => {
     const firstLink = await page.$("header div div a");
     await firstLink.click();
-    await page.waitForSelector('h1');
+    await page.waitForSelector("h1");
     const title2 = await page.title();
-    expect(title2).toEqual("GitHub for teams · Build like the best teams on the planet · GitHub");
+    expect(title2).toEqual(
+      "GitHub for teams · Build like the best teams on the planet · GitHub"
+    );
   }, 60000);
 
   test("The first link attribute", async () => {
-    const actual = await page.$eval("a", link => link.getAttribute('href') );
+    const actual = await page.$eval("a", (link) => link.getAttribute("href"));
     expect(actual).toEqual("#start-of-content");
   }, 60000);
 
-    test("The page contains Sign in button", async () => {
-      const btnSelector = ".btn-large-mktg.btn-mktg";
-      await page.waitForSelector(btnSelector, {
-        visible: true,
-      });
-      const actual = await page.$eval(btnSelector, link => link.textContent);
-      expect(actual).toContain("Get started with Team")
+  test("The page contains Sign in button", async () => {
+    const btnSelector = ".btn-large-mktg.btn-mktg";
+    await page.waitForSelector(btnSelector, {
+      visible: true,
     });
-  }, 60000);
+    const actual = await page.$eval(btnSelector, (link) => link.textContent);
+    expect(actual).toContain("Get started with Team");
+  });
+}, 60000);
 
-  
-  describe("Check Github", () => {
-
-    beforeEach(async () => {
-      page = await browser.newPage();
-     
-    });
-    
-    afterEach(() => {
-      page.close();
-    });
-
+describe("Check Github", () => {
   test("Click pricing", async () => {
     //page = await browser.newPage();
     await page.goto("https://github.com/pricing");
@@ -65,10 +54,10 @@ describe("Github page tests", () => {
   }, 60000);
 
   test("Trial plan", async () => {
-   // page = await browser.newPage();
+    // page = await browser.newPage();
     await page.goto("https://github.com/");
     await page.waitForSelector("h1");
     const title = await page.title();
     expect(title).toContain("Let’s build from here");
-  }, 60000)
-})
+  }, 60000);
+});
